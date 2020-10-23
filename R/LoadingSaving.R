192d02d44082ca55ec61f00daa2dcdb3f491d764
Schuemie <MSCHUEMI@its.jnj.com>
2020-10-23 05:26:24 +0200

    Reverting changes in DESCRIPTION and LoadingSaving.R

--- R/LoadingSaving.R
+++ R/LoadingSaving.R
@@ -53,21 +53,8 @@
 #' @export
 saveAndromeda <- function(andromeda, fileName, maintainConnection = FALSE, overwrite = TRUE) {
   if (!overwrite && file.exists(fileName)) {
-    msg <- paste0("File ", fileName, " already exists, and overwrite = FALSE")
-    rlang::abort(msg, class = "Andromeda")
+    stop("File ", fileName, " already exists, and overwrite = FALSE")
   }
-  
-  if (!isValidAndromeda(andromeda)) {
-    rlang::abort("andromeda object is closed or not valid.")
-  }
-  
-  fileName <- path.expand(fileName)
-  if (!dir.exists(dirname(fileName))) {
-    msg <- paste("The directory", dirname(fileName), "does not exist.\n",
-                 "Andromeda object cannot be saved to ", fileName, ".\n")
-    rlang::abort(msg, class = "Andromeda")
-  }
-  
   # Need to save any user-defined attributes as well:
   attribs <- attributes(andromeda)
   for (name in slotNames(andromeda)) {
@@ -88,7 +75,7 @@ saveAndromeda <- function(andromeda, fileName, maintainConnection = FALSE, overw
     RSQLite::dbDisconnect(andromeda)
     zip::zipr(fileName, c(attributesFileName, andromeda@dbname), compression_level = 2)
     unlink(andromeda@dbname)
-    rlang::inform("Disconnected Andromeda. This data object can no longer be used", class = "Andromeda")
+    writeLines("Disconnected Andromeda. This data object can no longer be used")
   }
   unlink(attributesFileName)
 }
@@ -156,36 +143,40 @@ loadAndromeda <- function(fileName) {
 }
 
 .checkAvailableSpace <- function(andromeda = NULL) {
-  warnDiskSpace <- getOption("warnDiskSpaceThreshold")
-  if (is.null(warnDiskSpace)) {
-    warnDiskSpace <- 10 * 1024 ^ 3
-  }
-  if (warnDiskSpace != 0) {
-    if (is.null(andromeda)) {
-      folder <- .getAndromedaTempFolder()
-    } else {
-      folder <- dirname(andromeda@dbname) 
+  if (.isInstalled("rJava")) {
+    warnDiskSpace <- getOption("warnDiskSpaceThreshold")
+    if (is.null(warnDiskSpace)) {
+      warnDiskSpace <- 10 * 1024 ^ 3
     }
-    if (exists("lowDiskWarnings", envir = andromedaGlobalEnv)) {
-      lowDiskWarnings <- get("lowDiskWarnings", envir = andromedaGlobalEnv)
-      if (folder %in% lowDiskWarnings) {
-        # Already warned about this location. Not warning again.
-        return()
+    if (warnDiskSpace != 0) {
+      if (is.null(andromeda)) {
+        folder <- .getAndromedaTempFolder()
+      } else {
+        folder <- dirname(andromeda@dbname) 
       }
-    } else {
-      lowDiskWarnings <- c()
-    }
-    space <- getAndromedaTempDiskSpace(andromeda)
-    if (!is.na(space) && space < warnDiskSpace) {
-      message <- sprintf("Low disk space in '%s'. Only %0.1f GB left.", 
-                         folder, 
-                         space / 1024^3)
-      
-      message <- c(message, 
-                   pillar::style_subtle("Use options(warnDiskSpaceThreshold = <n>) to set the number of bytes for this warning to trigger."))
-      
-      rlang::warn(paste(message, collapse = "\n"), class = "Andromeda", .frequency = "once", .frequency_id = folder) 
-      assign("lowDiskWarnings", c(lowDiskWarnings, folder), envir = andromedaGlobalEnv)
+      if (exists("lowDiskWarnings", envir = andromedaGlobalEnv)) {
+        lowDiskWarnings <- get("lowDiskWarnings", envir = andromedaGlobalEnv)
+        if (folder %in% lowDiskWarnings) {
+          # Already warned about this location. Not warning again.
+          return()
+        }
+      } else {
+        lowDiskWarnings <- c()
+      }
+      space <- getAndromedaTempDiskSpace(andromeda)
+      if (!is.na(space) && space < warnDiskSpace) {
+        message <- sprintf("Low disk space in '%s'. Only %0.1f GB left.", 
+                           folder, 
+                           space / 1024^3)
+        
+        message <- c(message, 
+                     pillar::style_subtle("Use options(warnDiskSpaceThreshold = <n>) to set the number of bytes for this warning to trigger."))
+        message <- c(message, 
+                     pillar::style_subtle("This warning will not be shown for this file location again during this R session."))
+        
+        warning(paste(message, collapse = "\n"), call. = FALSE) 
+        assign("lowDiskWarnings", c(lowDiskWarnings, folder), envir = andromedaGlobalEnv)
+      }
     }
   }
 }
@@ -194,6 +185,7 @@ loadAndromeda <- function(fileName) {
 #' 
 #' @description 
 #' Attempts to determine how much disk space is still available in the Andromeda temp folder. 
+#' This function uses Java, so will only work if the `rJava` package is installed.
 #' 
 #' By default the Andromeda temp folder is located in the system temp space, but the location
 #' can be altered using `options(andromedaTempFolder = "c:/andromedaTemp")`, where
@@ -205,8 +197,9 @@ loadAndromeda <- function(fileName) {
 #'
 #' @return
 #' The number of bytes of available disk space in the Andromeda temp folder. Returns NA
-#' if unable to determine the amount of available disk space, for example because
-#' because the user doesn't have the rights to query the available disk space.
+#' if unable to determine the amount of available disk space, for example because `rJava` 
+#' is not installed, or because the user doesn't have the rights to query the available 
+#' disk space.
 #'
 #' @examples
 #' # Get the number of available gigabytes:
@@ -218,56 +211,31 @@ getAndromedaTempDiskSpace <- function(andromeda = NULL) {
   if (!is.null(andromeda) && !inherits(andromeda, "SQLiteConnection")) 
     stop("Andromeda argument must be of type 'Andromeda'.")
   
-  if (is.null(andromeda)) {
-    folder <- .getAndromedaTempFolder()
+  # Using Java because no cross-platform functions available in R:
+  if (!.isInstalled("rJava")) {
+    return(NA)
   } else {
-    folder <- dirname(andromeda@dbname) 
-  }
-  folder <- normalizePath(folder)
-  
-  if (!dir.exists(folder)) {
-    msg <- paste("Andromeda temp folder does not exist\n",
-                 "folder:", folder, "\n",
-                 "Cannot determine available Andromeda disk space.")
-    rlang::warn(msg, class = "Andromeda")
-    return(NA)
-  }
-  
-  if (!(.Platform$OS.type %in% c("windows", "unix"))) {
-    msg <- "Operating system cannot be determined.\nCannot get available Andromeda disk space.\n"
-    rlang::warn(msg, class = "Andromeda")
-    return(NA)
-  }
-  
-  if (.Platform$OS.type == "windows") {
-    # https://stackoverflow.com/questions/32200879/how-to-get-disk-space-of-windows-machine-with-r
-    # https://stackoverflow.com/questions/293780/free-space-in-a-cmd-shell
-    driveLetter <- substr(folder, 1, 1)
-    space <- system(sprintf("wmic logicaldisk where caption=\"%s:\" get freespace", driveLetter),
-                    intern = TRUE)
-    if (!grepl("FreeSpace", space[1])) {
-      rlang::warn(paste0("Andromeda temp disk cannot be determined for folder ", folder), class = "Andromeda")
-      return(NA)
+    if (is.null(andromeda)) {
+      folder <- .getAndromedaTempFolder()
     } else {
-      space <- read.table(textConnection(space), 
-                          skip = 1,
-                          col.names =  c("freeSpace"),
-                          row.names = NULL)$freeSpace[1]
+      folder <- dirname(andromeda@dbname) 
     }
+    space <- tryCatch({
+      rJava::.jinit()
+      file <- rJava::.jnew("java.io.File", folder, check = FALSE, silent = TRUE)
+      rJava::.jcall(file, "J", "getUsableSpace")
+      
+      # This throws "illegal reflective access operation" warning:
+      # path <- rJava::J("java.nio.file.Paths")$get(fileName, rJava::.jarray(c("")))
+      # fileStore <- rJava::J("java.nio.file.Files")$getFileStore(path)
+      # fileStore$getUsableSpace()
+    }, error = function(e) NA)
+    return(space)
   }
-  
-  if (.Platform$OS.type == "unix") {
-    # https://stat.ethz.ch/pipermail/r-help/2007-October/142319.html
-    if (length(system("which df", intern = TRUE, ignore.stderr = TRUE)) != 1) {
-      msg <- paste0("`which df` command returned error\n",
-                    "Cannot get available Andromeda disk space.\n")
-      rlang::warn(msg, class = "Andromeda")
-      return(NA)
-    } else {
-      stdout <- system(paste("df", folder), intern = TRUE, ignore.stderr = TRUE)
-      space <- as.integer(strsplit(stdout[length(stdout)], "[ ]+")[[1]][4])
-    }
-  }
-  
-  return(space)
+}
+
+.isInstalled <- function(pkg) {
+  installedVersion <- tryCatch(utils::packageVersion(pkg), 
+                               error = function(e) NA)
+  return(!is.na(installedVersion))
 }
